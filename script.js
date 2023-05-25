13e911c865db71bd6ac4dd80965e1433f4e1e30b
AlexTimoncini <alextimonci@gmail.com>
2023-05-25 15:43:40 +0200

    added limit to the interval

--- script.js
+++ script.js
@@ -4,19 +4,27 @@ const minutesDom = document.getElementById('minutes');
 const secondsDom = document.getElementById('seconds');
 const nextLessonDate = new Date('May 26, 2023 09:30:00');
 
-setInterval(countdown, 1000, nextLessonDate, daysDom, hoursDom, minutesDom, secondsDom);
-
+countdown(nextLessonDate, daysDom, hoursDom, minutesDom, secondsDom);
 
 function countdown(finalDate, daysCounter, hoursCounter, minutesCounter, secondsCounter){
-    const today = new Date(Date.now());
-    const dateDifference = finalDate - today;
-    const days = dateDifference / (3600000 * 24);
-    const hours = (days - Math.floor(days)) * 24;
-    const minutes = (hours - Math.floor(hours)) * 60;
-    const seconds = (minutes - Math.floor(minutes)) * 60;
 
-    daysCounter.innerHTML = Math.floor(days);
-    hoursCounter.innerHTML = Math.floor(hours);
-    minutesCounter.innerHTML = Math.floor(minutes);
-    secondsCounter.innerHTML = Math.floor(seconds);
+    const liveCountdown = setInterval(timeCalc, 1000, finalDate, daysCounter, hoursCounter, minutesCounter, secondsCounter);
+
+    function timeCalc(finalDate, daysCounter, hoursCounter, minutesCounter, secondsCounter){
+        const today = new Date(Date.now());
+        const dateDifference = finalDate - today;
+        const days = dateDifference / (3600000 * 24);
+        const hours = (days - Math.floor(days)) * 24;
+        const minutes = (hours - Math.floor(hours)) * 60;
+        const seconds = (minutes - Math.floor(minutes)) * 60;    
+        if (dateDifference <= 0){
+            clearInterval(liveCountdown);
+        } else {
+            daysCounter.innerHTML = Math.floor(days);
+            hoursCounter.innerHTML = Math.floor(hours);
+            minutesCounter.innerHTML = Math.floor(minutes);
+            secondsCounter.innerHTML = Math.floor(seconds);
+        }
+    }
+
 }
