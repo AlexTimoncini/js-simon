d271cbf33df333c873dfd15ce4e87aef106419ad
AlexTimoncini <alextimonci@gmail.com>
2023-05-25 15:22:27 +0200

    added countdown function and setInterval of 1000ms

---
+++ script.js
@@ -0,0 +1,22 @@
+const daysDom = document.getElementById('days');
+const hoursDom = document.getElementById('hours');
+const minutesDom = document.getElementById('minutes');
+const secondsDom = document.getElementById('seconds');
+const nextLessonDate = new Date('May 26, 2023 09:30:00');
+
+setInterval(countdown, 1000, nextLessonDate, daysDom, hoursDom, minutesDom, secondsDom);
+
+
+function countdown(finalDate, daysCounter, hoursCounter, minutesCounter, secondsCounter){
+    const today = new Date(Date.now());
+    const dateDifference = finalDate - today;
+    const days = dateDifference / (3600000 * 24);
+    const hours = (days - Math.floor(days)) * 24;
+    const minutes = (hours - Math.floor(hours)) * 60;
+    const seconds = (minutes - Math.floor(minutes)) * 60;
+
+    daysCounter.innerHTML = Math.floor(days);
+    hoursCounter.innerHTML = Math.floor(hours);
+    minutesCounter.innerHTML = Math.floor(minutes);
+    secondsCounter.innerHTML = Math.floor(seconds);
+}
